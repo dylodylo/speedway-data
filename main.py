3ce62ce1502e9d14870a966e778e6c2e0d37f068
dylodylo <artur.dylewski.27@gmail.com>
2020-07-31 21:38:24 +0200

    divide main string to rider info (name, points etc.)

--- main.py
+++ main.py
@@ -13,19 +13,50 @@ def get_matches_from_season():
     return matches_links, matches
 
 matches_links, matches = get_matches_from_season()
-page = requests.get(matches_links[0])
-soup = BeautifulSoup(page.content, 'html.parser')
-scores = soup.find('p')
-scores = scores.find_all('tt')
-scores = scores[1].text
-scores = scores.split("\n")
-scores = [i.replace(u'\xa0', ' ') for i in scores]
-scores = [' '.join(i.split()) for i in scores]
-scores = [i.split() for i in scores]
-home_scores = [scores[1:9]]
-away_scores = [scores[10:18]]
-print(home_scores)
-print(away_scores)
+
+for match in matches_links:
+    page = requests.get(match)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    scores = soup.find('p')
+    scores = scores.find_all('tt')
+    scores = scores[1].text
+    scores = scores.split("\n")
+    scores = [i.replace(u'\xa0', ' ') for i in scores]
+    scores = [' '.join(i.split()) for i in scores]
+    scores = [i.split() for i in scores]
+    home_scores = scores[1:9]
+    away_scores = scores[10:18]
+    for i in home_scores[:]:
+        if i[1] == "brak" or i[2] == "ns":
+            home_scores.remove(i)
+
+    for i in away_scores[:]:
+        if i[1] == "brak" or i[2] == "ns":
+            away_scores.remove(i)
+    for rider in home_scores:
+        number = rider[0]
+        name = rider[1]
+        full_points = rider[2]
+        if "+" in full_points:
+            points = full_points.split("+")
+            bonuses = points[1]
+            points = points[0]
+        else:
+            points = full_points
+            bonuses = 0
+
+        details_points = rider[3]
+        heats = len(details_points.split(','))
+        print(name, full_points, points, bonuses, details_points, heats)
+
+    for rider in away_scores:
+        number = rider[0]
+        name = rider[1]
+        full_points = rider[2]
+        details_points = rider[3]
+        print(name, full_points)
+
+
 
 
 if __name__ == "main":
