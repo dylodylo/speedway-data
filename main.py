21454b57caf97127e4caaa27f67bb820cd799a02
dylodylo <artur.dylewski.27@gmail.com>
2020-08-03 20:29:05 +0200

    split code to functions (get_riders_points, get_all_scores, scrapping), add database connection

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import psycopg2
+from config import config
 
-def get_matches_from_season():
-    page = requests.get("http://www.speedwayw.pl/pl_2019.htm")
+
+def get_matches_from_season(site):
+    page = requests.get(site)
     soup = BeautifulSoup(page.content, 'html.parser')
     rounds = soup.find('p', align="left")
     matches_links = rounds.find_all('a')
@@ -12,28 +15,13 @@ def get_matches_from_season():
     print(matches_links)
     return matches_links, matches
 
-matches_links, matches = get_matches_from_season()
 
-for match in matches_links:
-    page = requests.get(match)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    scores = soup.find('p')
-    scores = scores.find_all('tt')
-    scores = scores[1].text
-    scores = scores.split("\n")
-    scores = [i.replace(u'\xa0', ' ') for i in scores]
-    scores = [' '.join(i.split()) for i in scores]
-    scores = [i.split() for i in scores]
-    home_scores = scores[1:9]
-    away_scores = scores[10:18]
-    for i in home_scores[:]:
+def get_riders_points(riders_scores):
+    for i in riders_scores[:]:
         if i[1] == "brak" or i[2] == "ns":
-            home_scores.remove(i)
+            riders_scores.remove(i)
 
-    for i in away_scores[:]:
-        if i[1] == "brak" or i[2] == "ns":
-            away_scores.remove(i)
-    for rider in home_scores:
+    for rider in riders_scores:
         number = rider[0]
         name = rider[1]
         full_points = rider[2]
@@ -49,15 +37,74 @@ for match in matches_links:
         heats = len(details_points.split(','))
         print(name, full_points, points, bonuses, details_points, heats)
 
-    for rider in away_scores:
-        number = rider[0]
-        name = rider[1]
-        full_points = rider[2]
-        details_points = rider[3]
-        print(name, full_points)
+
+def get_all_scores(page_content):
+    scores = page_content.find('p')
+    scores = scores.find_all('tt')
+    scores = scores[1].text
+    scores = scores.split("\n")
+    scores = [i.replace(u'\xa0', ' ') for i in scores]
+    scores = [' '.join(i.split()) for i in scores]
+    scores = [i.split() for i in scores]
+    team1_scores = scores[1:9]
+    team2_scores = scores[10:18]
+    return team1_scores, team2_scores
 
 
+def get_teams(page_content):
+    teams = page_content.find('title')
+    teams = teams.text.split(' - ')
+    team1 = teams[0]
+    team2 = teams[1]
+    return team1, team2
 
 
-if __name__ == "main":
+def scrapping():
+    matches_links, matches = get_matches_from_season("http://www.speedwayw.pl/pl_2019.htm")
+
+    for match in matches_links:
+        page = requests.get(match)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        home_scores, away_scores = get_all_scores(soup)
+        home_team, away_team = get_teams(soup)
+        print(home_team, away_team)
+        get_riders_points(home_scores)
+        get_riders_points(away_scores)
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+if __name__ == "__main__":
+    connect()
+    scrapping()
     pass
