2f87bb139e0c9599560c5c1fa26099f35feb6cc5
dylodylo <artur.dylewski.27@gmail.com>
2020-08-04 19:31:03 +0200

    add database functions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import psycopg2
 from config import config
+import database
 
 
 def get_matches_from_season(site):
@@ -72,39 +73,9 @@ def scrapping():
         get_riders_points(away_scores)
 
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
 if __name__ == "__main__":
-    connect()
-    scrapping()
+    #database.create_tables()
+    database.insert_match(('Bydgoszcz', 'Toruń', '1liga', '2020','2021-08-20'))
+    print(database.get_match_id(('Bydgoszcz', '2021-08-20')))
+    #scrapping()
     pass
